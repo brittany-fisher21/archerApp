53a082bcaf3dc7f85acfaa6c5d186b42ab06f8c8
Brittany Fisher <bribreana7@gmail.com>
2021-07-07 14:59:30 -0400

    added photos, packages, and appjs

---
+++ sql/seed.sql
@@ -0,0 +1,17 @@
+INSERT INTO users
+(name, email, user_name, password)
+VALUES
+();
+
+
+INSERT INTO locations
+    (location_name, location_img, location_description, location_activities)
+VALUES
+    ();
+
+    INSERT INTO activities
+    (activity_name, activity_level, activity_terrain)
+    VALUES
+    ('hiking, moderate, unpaved');
+    ('kayaking, easy, river');
+    ('backpacking, difficult, ')
