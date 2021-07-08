68ca580f267eba44f25c1ca4822f68200cb35dc6
Brittany Fisher <bribreana7@gmail.com>
2021-07-08 16:38:50 -0400

    expanding on schema and seed data

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
     id serial PRIMARY KEY,
     name VARCHAR (20) NOT NULL,
     email VARCHAR (40) NOT NULL,
-    user_name, VARCHAR (40) NOT NULL,
+    user_name VARCHAR (40) NOT NULL,
     password VARCHAR (10) NOT NULL
 );
 
@@ -10,18 +10,22 @@ CREATE TABLE locations (
     id serial PRIMARY KEY,
     state_name text,
     location_name text,
-    location_img text,
     location_description text,
     location_activities text
 );
 
 CREATE TABLE activities (
+    id serial PRIMARY KEY,
     activity_name text,
     activity_level text,
-    activity_terrain text,
+    activity_terrain text
 );
 
 
 CREATE TABLE reviews (
-
+    id serial PRIMARY KEY,
+    score integer,
+    content text,
+    locations_id integer REFERENCES locations(id),
+    user_id integer REFERENCES users (id)
 );
--- sql/seed.sql
+++ sql/seed.sql
@@ -1,17 +1,35 @@
 INSERT INTO users
-(name, email, user_name, password)
+(name, email, user_name)
 VALUES
-();
+('Alyna', 'alyna@fakeemail.com', 'Alyna_fake');
+
+('Brittany', 'brittany@fakeemail.com', 'Brittany_fake');
+
+('Joshua', 'joshua@fakeemail.com', 'Joshua_fake');
 
 
 INSERT INTO locations
-    (location_name, location_img, location_description, location_activities)
+    (state_name, location_name, location_description, location_activities)
 VALUES
-    ();
+     ('Texas', 'Lake Allatoona', 'Peaceful and calm waters', 'Kayaking'),
+
+    ('Texas', 'Good Water Loop',  'Features a lake and 26.9 trail', 'Backpacking and Hiking'),
+
+    ('Texas', 'Barnwell Mountain Recreation Area Loop', 'Wooded area with dirt trails and shallow water crossings', 'OHV');
+
 
     INSERT INTO activities
     (activity_name, activity_level, activity_terrain)
     VALUES
-    ('hiking, moderate, unpaved');
-    ('kayaking, easy, river');
-    ('backpacking, difficult, ')
+    ('hiking', 'moderate', 'unpaved'),
+    ('kayaking', 'easy', 'river'),
+    ('backpacking', 'difficult', 'varrying terrian');
+
+    INSERT INTO reviews
+    (score, content, locations_id, user_id)
+    VALUES
+    (5, 'Soooo much fun', 1, 1 ),
+
+    (5, 'Amazing experience! Beautiful and Peaceful', 2, 2),
+
+    (5, 'This place is a gem! Grateful to have found this trail', 3, 3);
