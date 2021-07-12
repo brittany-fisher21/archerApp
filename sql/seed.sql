dd260555ae40c7572ad27f120c47e87b800b115f
Alyna Palamarchuk <alynapchuk@gmail.com>
2021-07-12 09:38:30 -0500

    update SQL

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,37 +1,68 @@
 INSERT INTO users
+
     (name, email, user_name, password)
+
 VALUES
-    ('Alyna', 'alyna@fakeemail.com', 'Alyna_fake', 'abc'),
-    ('Brittany', 'brittany@fakeemail.com', 'Brittany_fake', 'xyz'),
-    ('Joshua', 'joshua@fakeemail.com', 'Joshua_fake', '123')
+    ('Alyna', 'alyna@fakeemail.com', 'alyna_fake', 'passA'),
+
+    ('Brittany', 'brittany@fakeemail.com', 'brittany_fake', 'passB'),
+
+    ('Joshua', 'joshua@fakeemail.com', 'joshua_fake', 'passC')
 
 INSERT INTO locations
+
     (state_name, location_name, location_description, location_image location_activities)
+
 VALUES
-    ('Texas', 'Lake Allatoona', 'Peaceful and calm waters', '/images/kayaking_lake_allatoona.jpg', 'Kayaking'),
-    ('Texas', 'Good Water Loop',  'Features a lake and 26.9 trail', '/images/???', 'Backpacking and Hiking'),
-    ('Texas', 'Barnwell Mountain Recreation Area Loop', 'Wooded area with dirt trails and shallow water crossings', '/images/barnewell_mountain_ohv.jpg', 'OHV');
+
+    ('Texas', 'Lake Allatoona', 'Peaceful and calm waters', '/images/#', 'Kayaking'),
+
+    ('Texas', 'Good Water Loop',  'Features a lake and 26.9 trail', '/images/#', 'Backpacking and Hiking'),
+
+    ('Texas', 'Barnwell Mountain Recreation Area Loop', 'Wooded area with dirt trails and shallow water crossings', '/images/#', 'OHV');
 
 INSERT INTO activities
+
     (activity_name, activity_level, activity_terrain)
+
 VALUES
+
     ('hiking', 'moderate', 'unpaved'),
+
     ('kayaking', 'easy', 'river'),
+
     ('backpacking', 'difficult', 'varrying terrian');
 
--- BOOKINGS --
+INSERT INTO bookings
+
+    (booking_name, booking_location booking_description, booking_image, booking_price)
+
+VALUES
+
+    ('name', 'location', 'description', '/images/#', 99),
+
+    ('name', 'location', 'description', '/images/#', 99),
+
+    ('name', 'location', 'description', '/images/#', 99);
 
 INSERT INTO reviews
+
     (score, content, locations_id)
+
 VALUES
+
     (5, 'Soooo much fun', 1),
+
     (5, 'Amazing experience! Beautiful and Peaceful', 2),
+
     (5, 'This place is a gem! Grateful to have found this trail', 3);
 
--- NEED TO ADD ITINERARY BUT YA GIRL IS SLEEPY --
 INSERT INTO itinerary
+
     (locations_id, activities_id, bookings_id, reviews_id)
+
 VALUES
+
 (),
 (),
 ();
